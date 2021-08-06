2039dc19f7c7049b61a2457fbeaba51f5a5a630a
Raian Medeiros Nolaço <raiannolaco@gmail.com>
2021-08-06 01:56:29 -0300

    delete

--- ByteBank/src/e/Cliente.java
+++ ByteBank/src/e/Cliente.java
@@ -1,3 +1,4 @@
+package e;
 
 public class Cliente {
 
---
+++ ByteBank/src/e/TesteBanco.java
@@ -0,0 +1,26 @@
+package e;
+
+public class TesteBanco {
+
+	
+	public static void main(String[] args) {
+		
+		Cliente raian = new Cliente();
+		
+		raian.nome = "Raian Medeiros";
+		
+		raian.cpf = "639.990.559-73";
+		
+		raian.profissao = "Programador";
+		
+		Conta contaRaian = new Conta();
+		
+		contaRaian.depositar(100);
+		
+		contaRaian.titular = raian;
+		
+		System.out.println(contaRaian.titular.profissao);
+		
+	}
+	
+}
